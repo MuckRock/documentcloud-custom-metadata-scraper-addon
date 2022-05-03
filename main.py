b9cabfdb47b21c45b14bc5ec9fe741a7729f11ed
Dave Godfrey <rocketdave2564@gmail.com>
2022-05-03 12:42:29 -0400

    formatted with black

--- main.py
+++ main.py
@@ -10,19 +10,21 @@ import sys
 from documentcloud.addon import AddOn
 import csv
 
+
 class CustomMetaData(AddOn):
     """An metadata scraping Add-On for DocumentCloud."""
-    def main(self):     
+
+    def main(self):
 
         if not self.documents:
             self.set_message("Please select at least one document")
             return
 
         self.set_message("Custom MetaData scraping starting!")
-        
-        #getting the input from the frontend, it will be a dictionary of variable length
-        #containing the data types the user wants
-        #parse through and set the index value in the values array to 1 for every wanted data type 
+
+        # getting the input from the frontend, it will be a dictionary of variable length
+        # containing the data types the user wants
+        # parse through and set the index value in the values array to 1 for every wanted data type
         values = [0] * 12
         for data in self.data:
             if self.data.get(data) == True:
@@ -50,69 +52,89 @@ class CustomMetaData(AddOn):
                     values[10] = 1
                 elif data == "KEYVALUEPAIRS":
                     values[11] = 1
-  
-        # preset header + metadata list
-        header = ['Id', 'Title', 'Privacy Level', 'Asset-Url', 
-        'Contributor', 'Created at Date', 'Description' ,'Full-Text-Url', 'Pdf-Url',
-        'Page Count', 'Tags', 'Key Value Pairs']
 
-        #delete values of 0 from the header because the user does not want them 
+        # preset header + metadata list
+        header = [
+            "Id",
+            "Title",
+            "Privacy Level",
+            "Asset-Url",
+            "Contributor",
+            "Created at Date",
+            "Description",
+            "Full-Text-Url",
+            "Pdf-Url",
+            "Page Count",
+            "Tags",
+            "Key Value Pairs",
+        ]
+
+        # delete values of 0 from the header because the user does not want them
         Newheader = []
         for head in header:
-            #differentiate data types by index
+            # differentiate data types by index
             if values[header.index(head)] == 1:
                 Newheader.append(head)
 
-        metadata_list = [] # list of lists containing metadata for each document
+        metadata_list = []  # list of lists containing metadata for each document
 
-        #takes the document object and an empty array as input, and places the document metadata into the array
+        # takes the document object and an empty array as input, and places the document metadata into the array
         def setData(doc, doc_metadata):
 
-            #document description break fix
+            # document description break fix
             try:
                 description = doc.description
             except AttributeError:
-                 description = ""
-            
-            doc_metadata = [doc.id, doc.title, doc.access, doc.asset_url,
-            doc.contributor, doc.created_at, description, doc.full_text_url,
-            doc.pdf_url, doc.page_count]
+                description = ""
 
-            #separate key values and tags into two separate arrays
+            doc_metadata = [
+                doc.id,
+                doc.title,
+                doc.access,
+                doc.asset_url,
+                doc.contributor,
+                doc.created_at,
+                description,
+                doc.full_text_url,
+                doc.pdf_url,
+                doc.page_count,
+            ]
+
+            # separate key values and tags into two separate arrays
             keyvalues = doc.data
             tags = ""
 
-            #are there any tags?
+            # are there any tags?
             try:
-                tags = keyvalues['_tag']
-                del keyvalues['_tag']
+                tags = keyvalues["_tag"]
+                del keyvalues["_tag"]
             except KeyError:
                 tags = ""
-            
+
             doc_metadata.append(tags)
             doc_metadata.append(keyvalues)
 
             return doc_metadata
 
         # retrieve information from each document.
-        description = "NO DESCRIPTION PRESENT" #for the edge case with the description not existing
+        description = "NO DESCRIPTION PRESENT"  # for the edge case with the description not existing
         if self.documents:
             length = len(self.documents)
             for doc_id in self.documents:
                 doc = self.client.documents.get(doc_id)
 
-                #set the metadata
-                metadata_list.append(setData(doc,[]))
+                # set the metadata
+                metadata_list.append(setData(doc, []))
         elif self.query:
             i = 0
             documents = self.client.documents.search(self.query)
             for document in documents:
                 # set the metadata
-                metadata_list.append(setData(document,[]))
-                i+=1
+                metadata_list.append(setData(document, []))
+                i += 1
             length = i
 
-        #go through the accumulated data and delete the data the user does not want from EACH document
+        # go through the accumulated data and delete the data the user does not want from EACH document
         Newdatalist = []
         for document_data in metadata_list:
             wantedData = []
@@ -128,19 +150,22 @@ class CustomMetaData(AddOn):
             firstID = ""
             length = 1
 
-        with open("data_for_"+str(firstID)+"+"+str(length-1)+".csv", "w+") as file_:
+        with open(
+            "data_for_" + str(firstID) + "+" + str(length - 1) + ".csv", "w+"
+        ) as file_:
             writer = csv.writer(file_)
 
-            #FORMAT HEADER
+            # FORMAT HEADER
             writer.writerow(Newheader)
 
             for row in Newdatalist:
-                #FORMAT THE DATA 
+                # FORMAT THE DATA
                 writer.writerow(row)
-            
+
             self.upload_file(file_)
 
         self.set_message("Custom MetaData scraping finished!")
+
 
 if __name__ == "__main__":
     CustomMetaData().main()
